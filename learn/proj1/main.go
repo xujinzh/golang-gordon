1131b7f69085980223dcd704cc6a701061d04ede
Jinzhong Xu <xujinzhong027@icloud.com>
2023-10-22 21:59:43 +0800

    add file write and read

--- learn/proj1/main.go
+++ learn/proj1/main.go
@@ -1,14 +1,39 @@
 package main
-
 import (
-	"fmt"
-	misc "proj1/misc"
-	proj2Misc "proj2/misc"
-	_ "unsafe"
-	_ "strconv"
+    "encoding/gob"
+    "fmt"
+    "os"
 )
-
 func main() {
-	fmt.Println(misc.Clock)
-	fmt.Println(proj2Misc.SayHello)
+    info := "how are you?"
+    file, err := os.Create("./data/btext.gob")
+    if err != nil {
+        fmt.Println("文件创建失败", err.Error())
+        return
+    }
+    defer file.Close()
+    encoder := gob.NewEncoder(file)
+    err = encoder.Encode(info)
+    if err != nil {
+        fmt.Println("编码错误", err.Error())
+        return
+    } else {
+        fmt.Println("编码成功")
+    }
+
+	file1, err1 := os.Open("./data/btext.gob")
+    if err1 != nil {
+        fmt.Println("文件打开失败", err1.Error())
+        return
+    }
+    defer file1.Close()
+    decoder := gob.NewDecoder(file1)
+    info1 := ""
+    err1 = decoder.Decode(&info1)
+    if err1 != nil {
+        fmt.Println("解码失败", err1.Error())
+    } else {
+        fmt.Println("解码成功")
+        fmt.Println(info)
+    }
 }
