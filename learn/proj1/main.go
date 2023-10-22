bb84248a02446b02c2c22b389ecda1aac493836b
Jinzhong Xu <xujinzhong027@icloud.com>
2023-10-22 18:04:10 +0800

    change proj2 package name in proj1 main

--- learn/proj1/main.go
+++ learn/proj1/main.go
@@ -3,12 +3,12 @@ package main
 import (
 	"fmt"
 	misc "proj1/misc"
-	hi "proj2/misc"
+	proj2Misc "proj2/misc"
 	_ "unsafe"
 	_ "strconv"
 )
 
 func main() {
 	fmt.Println(misc.Clock)
-	fmt.Println(hi.SayHello)
+	fmt.Println(proj2Misc.SayHello)
 }
