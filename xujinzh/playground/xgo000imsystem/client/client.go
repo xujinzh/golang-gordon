175526b91c1e9fd0bb6ebea35c3ad3f12ab33c9e
jinzhongxu <jinzhongxu@csu.ac.cn>
2026-01-20 19:32:24 +0800

    修复用户在客户端输入回车导致退出程序的问题

--- xujinzh/playground/xgo000imsystem/client/client.go
+++ xujinzh/playground/xgo000imsystem/client/client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -39,10 +41,12 @@ func (this *Client) Menu() bool {
 	fmt.Println("1. 公聊模式")
 	fmt.Println("2. 私聊模式")
 	fmt.Println("3. 更新用户名")
-	fmt.Println("0. 退出")
-
+	fmt.Println("4. 退出")
+	fmt.Println(">>>请输入1-4之间的整数：<<<")
 	fmt.Scanln(&flagMenu)
-	if flagMenu >= 0 && flagMenu <= 3 {
+
+	// fmt.Println("-----------------------flagMenu:", flagMenu)
+	if flagMenu >= 1 && flagMenu <= 4 {
 		this.flagMenu = flagMenu
 		return true
 	} else {
@@ -71,22 +75,35 @@ func (this *Client) UpdateName() bool {
 	return true
 }
 
+// 处理server回应的消息，显示到标准输出
+func (this *Client) DealResponse() {
+	// 把 conn 中的消息拷贝到 os.stdout 中，永久阻塞
+	// 一旦client.conn有数据，就直接copy到stdout标准输出上，永久阻塞监听（不是只拷贝一次，第二次不拷贝了）
+	io.Copy(os.Stdout, this.conn)
+}
+
 // 客户端主业务
 func (this *Client) Run() {
-	for this.flagMenu != 0 {
+	fmt.Println("flagMenu:", this.flagMenu)
+	for this.flagMenu != 4 {
+		// 如果用户在客户端输入的不是1-4之间的数字，那么会一直请用户输入，直到输入1-4之间的值
+		// 下面这个for循环保证用户只能输入1-4之间的整数
 		for this.Menu() != true {
-
+			// fmt.Println("this.menu is not true")
 		}
+		// 当用户输入1-4之前的某个值时，进入下面的代码，选择功能
+		// 当用户输入4时，因为不符合下面的选项，会判断外层for循环的判断，不满足条件则退出
 		// 根据不同模式处理不同业务
 		switch this.flagMenu {
 		case 1: // 公聊模式
-			fmt.Println("公聊模式...")
+			fmt.Println("----------------公聊模式...")
 		case 2: // 私聊模式
-			fmt.Println("私聊模式...")
+			fmt.Println("----------------私聊模式...")
 		case 3: // 更新用户名
 			// fmt.Println("更新用户名...")
 			this.UpdateName()
 		}
+
 	}
 }
 
@@ -107,6 +124,9 @@ func main() {
 		fmt.Println(">>>>>>连接服务器失败...")
 		return
 	}
+	// 单独开启一个 goroutin 去处理 server 发送的消息
+	go client.DealResponse()
+
 	fmt.Println(">>>>>>连接服务器成功...")
 	// 启动客户端业务
 	client.Run()
